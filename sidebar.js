054ad78703e3ff4e9f395ce6c9be885d5f0d8565
neko, I am a neko <81370963+nealnyethebadguy@users.noreply.github.com>
2021-03-26 10:25:01 -0400

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,4 @@
+function loadNavbarDiv() {
+     String navbar_code_str = '<div class="sidenav" id="sidebar" style="display: none"><a href=""></a><a href=""></a><a href="/">Home</a><a href="/performance-assessment">current school project</a><a href="https://suicidepreventionlifeline.org/talk-to-someone-now/">I wanna die</a></div>'
+     $('body').append(navbar_code_str);
+}
