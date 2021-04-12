03fcec57cbd52cde2edcd49cf783f6ed58d60686
neko, I am a neko <81370963+nealnyethebadguy@users.noreply.github.com>
2021-04-12 07:00:43 -0400

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
 function loadNavbarDiv() {
-     const navbar_code_str = '<div class="sidenav" id="sidebar" style="display: none"><a href=""></a><a href=""></a><a href="/">Home</a><a href="/performance-assessment">current school project</a><a href="https://suicidepreventionlifeline.org/talk-to-someone-now/">I wanna die</a></div>'
+     const navbar_code_str = '<div class="sidenav" id="sidebar" style="display: none"><a href=""></a><a href=""></a><a href="/">Home</a><a href="/performance-assessment">performance assessment</a><a href="https://suicidepreventionlifeline.org/talk-to-someone-now/">I wanna die</a></div>'
      $('body').append(navbar_code_str);
 }
