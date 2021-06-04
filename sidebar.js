50a1b245247191b01217e2c10f04c19fe11f724f
neko, I am a neko <81370963+nealnyethebadguy@users.noreply.github.com>
2021-06-04 00:53:24 -0400

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
 function loadNavbarDiv() {
-     const navbar_code_str = '<div class="sidenav" id="sidebar" style="display: none"><a href=""></a><a href=""></a><a href="/">Home</a><a href="/performance-assessment">performance assessment</a></div>'
+     const navbar_code_str = '<div class="sidenav" id="sidebar" style="display: none"><a href=""></a><a href=""></a><a href="/">Home</a><a href="/performance-assessment">performance assessment</a><a href="/project-homestead/">Project Homestead</a></div>'
      $('body').append(navbar_code_str);
 }
